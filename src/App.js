bc888f6ae4cb3bb7dd03d64a38438045b44a9205
BlessAdjorlolo <adjorlolobless11@gmail.com>
2022-08-09 14:11:32 +0000

    first react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from "./component/Header";
+import { Button1, Button2, Button3 } from "./component/Button";
+import { Image } from "./component/Image";
+
+function App(){
+  return (
+    <div>
+      <Header />
+      <Button1 />
+      <Button2 />
+      <Button3 />
+      <Image />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Image.jsx
@@ -0,0 +1,4 @@
+import flyer from "../assets/Img/Shine-Church-Flyer-Poster-Photoshop2.webp";
+export const Image = () => {
+    return <img src={flyer} alt="" />;
+};
